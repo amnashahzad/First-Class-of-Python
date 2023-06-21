e19485dab556e01251d44c382f29aa0ad9977304
Amna Shahzad <121329321+amnashahzad@users.noreply.github.com>
2023-06-21 15:45:08 +0500

    Add files via upload

---
+++ FirstClass.py
@@ -0,0 +1,89 @@
+                                                     # Variables
+# name = "Amna"
+# age = 22
+# print(name)
+# print(age)
+# print("Hello World")
+                                                      # Exercise Variables
+# person = "Tony Stark"
+# age = 51
+# genius = "Tony is a genius"
+# is_genius = True
+# print(person)
+# print(age)
+# print(genius)
+# print(is_genius)
+
+                                                     # Input
+
+# input("What is your Name") # is not store
+# name = input("What is your Name") # is store
+# print(name)
+
+# name = input("What is your Name") # is store
+# print("Hello" + name)
+
+                                                     # Exercise Input
+
+# superhero = input("What is your superhero name ")
+# print(superhero)
+#
+# actress = input(" You watch drama so your favourite actress " "What is your favourite actress name ")
+# print(actress)
+
+                                                     # Type Conversion
+
+# old_age = input("Enter your age : ")
+# #new_age = old_age + 2
+# #print(new_age)
+
+# new_age = int(old_age) + 2
+# print(new_age)
+
+# first_number = input("Enter 1st number : ")
+# second_number = input("Enter 2nd number : ")
+# sum = float(first_number) + float(second_number)
+# print("the sum is : " + str(sum))
+
+                                                     # Strings
+
+# name = "Tony Stark"
+# print(name.upper())
+# print(name)
+# print(name.lower())
+# print(name)
+# print() print is keyword
+# print(name.find('y'))
+# print(name.find('Y'))
+# print(name.find("Stark"))
+# print(name.find("stark"))
+#
+# print(name.replace("Tony Stark", "Ironman"))
+# print(name)
+# #to check if a character/string is part of the main string
+# print("Stark" in name)
+# print("S" in name)
+# print("s" in name)
+
+                                                     # Arithmetic Operators
+
+# + - / * % ( # this is  Operators
+
+
+print(5 + 2)
+print(5 - 2)
+print(5 * 2)
+print(5 / 2)
+print( 5 // 2)
+print(5 % 2)
+print(5 ** 2)
+i = 5
+i = i + 2
+i += 2
+i -= 2
+i *= 2
+
+
+
+
+
